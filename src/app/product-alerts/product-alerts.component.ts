0d93fd50b3e5bd51e6f853ed218e6917a3c3c43e
Grenadator <grenadator@gmail.com>
2019-06-16 12:52:41 +0200

    Push current work

---
+++ src/app/product-alerts/product-alerts.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { Input } from '@angular/core';
+import { Output, EventEmitter } from '@angular/core';
+
+@Component({
+  selector: 'app-product-alerts',
+  templateUrl: './product-alerts.component.html',
+  styleUrls: ['./product-alerts.component.css']
+})
+export class ProductAlertsComponent {
+  // The @Input decorator indicates that the property value will be passed in from the component's parent
+  @Input() product;
+  @Output() notify = new EventEmitter();
+
+}
